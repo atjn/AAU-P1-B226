dba1ea0918b60d0d92b2b029c9a49819e05ef562
Jesper276 <jespermadsenvejle@hotmail.dk>
2021-11-03 12:53:03 +0100

    Lavede en ny function
    Jesper function er sej

--- function.c
+++ function.c
@@ -7,9 +7,19 @@
 int function(){
     return 1;
 }
+int jespersFunction(){
+    return 3;
+}
+
 
 void testFunction1(CuTest *tc){
     int actual = function();
     int expected = 1;
+    CuAssertIntEquals(tc, expected, actual);
+}
+
+void testJespersFunction(CuTest *tc){
+    int actual = jespersFunction();
+    int expected = 2;
     CuAssertIntEquals(tc, expected, actual);
 }
