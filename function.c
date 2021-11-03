122826954ffad1bdc0f2d6e93f1d61be5e10b705
Bastian Brix Hansen <bastian.b.hansen@hotmail.com>
2021-11-03 12:27:30 +0100

    TestFunction

--- function.c
+++ function.c
@@ -1,4 +1,6 @@
 #include "lib/CuTest-AAU/CuTest.h"
+#include <stdio.h>
+#include <math.h>
 
 /**
  * Sample function - this is what a function looks like with an included function test.
@@ -10,6 +12,19 @@ int function(){
 
 void testFunction1(CuTest *tc){
     int actual = function();
+    int expected = 1;
+    CuAssertIntEquals(tc, expected, actual);
+}
+
+int brixFunction() {
+    for (int i = 0; i < pow(10, 2); i++) {
+        printf("Abekat %d\n", i);
+    }
+    return 1;
+}
+
+void testFunction2(CuTest *tc){
+    int actual = brixFunction();
     int expected = 1;
     CuAssertIntEquals(tc, expected, actual);
 }
