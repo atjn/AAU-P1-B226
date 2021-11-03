7cae39e7bea200b97c8b1b57beabb205d67fd869
root <root@Arnar-Laptop.localdomain>
2021-11-03 13:09:14 +0100

    made gaming

--- function.c
+++ function.c
@@ -1,10 +1,19 @@
 #include "lib/CuTest-AAU/CuTest.h"
-#include <stdio.h>
-#include <math.h>
 
 /**
  * Sample function - this is what a function looks like with an included function test.
  */
+
+
+int gaming(){
+    return 3;
+}
+
+void testGaming(CuTest *tc){
+    int actual = gaming();
+    int expected = 3;
+    CuAssertIntEquals(tc, expected, actual);
+}
 
 int function(){
     return 1;
