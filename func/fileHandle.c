d06ac5bfd65891bc3e8a7e47f6ea7fd6a2729573
BaBrixx <bastian.b.hansen@gmail.com>
2021-11-12 09:09:30 +0100

    Chancged getline to fgets WINDOWS COMP?

--- func/fileHandle.c
+++ func/fileHandle.c
@@ -11,38 +11,38 @@
 void readFile() {
     /* Tempoary variabls for reading the recipe file */
     FILE *fp;
-    char *line = NULL;
-    size_t len = 0;
-    size_t read;
-    char ingredientDelim[] = ";";
+    char line[60];
+    int len = 60;
+    //char ingredientDelim[] = ";";
 
     /* Other variables */
     int recipesNum = -1;
-    Recipe *loadedRecipes;
+    //Recipe *loadedRecipes;
 
     /* Opening the recipes file */
     fp = fopen(RECIPES_LOCATION, "r");
     if (fp == NULL) exit(EXIT_FAILURE);
-
+    
     /* Reading the file to get the number of recipes */
-    while ((read = getline(&line, &len, fp)) != (unsigned long) -1) {
+    while (fgets(line, len, fp) != NULL) {
         if (line[0] != ';') {
             recipesNum++;
         }
     }
-
+    
     /* Allocating memory for the loaded recipes array */
+    /*
     loadedRecipes = malloc(recipesNum * sizeof(Recipe));
 
     int currRecipeIndex = -1;
     int currLine = 0;
     int tempIngredientCount = 0;
-
+    */
     /* Rewinding the file to start reading it again */
     rewind(fp);
 
     /* Reading the file to get the number of recipes */
-    while ((read = getline(&line, &len, fp)) != (unsigned long) -1) {
+    /*while ((read = getline(&line, &len, fp)) != (unsigned long) -1) {
         if (currLine > 0) {
             if (line[0] != ';') {
                 currRecipeIndex++;
@@ -59,37 +59,18 @@ void readFile() {
                     }
 		            ingredientPtr = strtok(NULL, ingredientDelim);
 	            }
-
-                /* 
-                tempIngredientCount++;
-                int srcFor = 0;
-                for (int i = 1; i < read; i++) {
-                    if (line[i] == ';') {
-                        loadedRecipes[currRecipeIndex].ingredients[tempIngredientCount].ingredientName[i] = '\0';
-                        strcat(loadedRecipes[currRecipeIndex].recipeName, "\0");
-                        srcFor++;
-                    }
-                    else {
-                        if (srcFor == 0) {
-                            printf("%d and %c\n", i, line[i]);
-                            loadedRecipes[currRecipeIndex].ingredients[tempIngredientCount].ingredientName[i] = line[1];
-                        }
-                    }
-                }
-                */
             }
         }
         currLine++;
-    }
+    }*/
 
-    printf("%s\n", loadedRecipes[0].recipeName);
-    printf("%s\n", loadedRecipes[1].recipeName);
-    printf("%c\n", loadedRecipes[0].ingredients[0].ingredientName[2]);
+    //printf("%s\n", loadedRecipes[0].recipeName);
+    //printf("%s\n", loadedRecipes[1].recipeName);
+    //printf("%c\n", loadedRecipes[0].ingredients[0].ingredientName[2]);
 
-    free(loadedRecipes);
+    //free(loadedRecipes);
 
     printf("%d", recipesNum);
 
     fclose(fp);
-    if (line) free(line);
 }
