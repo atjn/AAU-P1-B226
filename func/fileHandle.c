2c237f18e4850bd73ef0f45855e23dddef81b364
AlbertHald <albert24122@gmail.com>
2021-11-12 12:08:58 +0100

    Wrote comments!

--- func/fileHandle.c
+++ func/fileHandle.c
@@ -20,14 +20,14 @@ Recipe * readFile(int *recipesNumber) {
     /* Opening the recipes file */
     fp = fopen(RECIPES_LOCATION, "r");
     if (fp == NULL) exit(EXIT_FAILURE);
-    
+
     /* Reading the file to get the number of recipes */
     while (fgets(line, len, fp) != NULL) {
         if (line[0] != ';') {
             recipesNum++;
         }
     }
-    
+
     /* Allocating memory for the loaded recipes array */
     loadedRecipes = malloc(recipesNum * sizeof(Recipe));
 
@@ -35,35 +35,39 @@ Recipe * readFile(int *recipesNumber) {
     int currLine = 0;
     int tempIngredientCount = 0;
     int tempIngredientInfoCount = 0;
-    
+
     /* Rewinding the file to start reading it again */
     rewind(fp);
 
     /* Reading the file to get the number of recipes */
     while (fgets(line, len, fp) != NULL) {
-        /* Skipping the first line containt CSV metadata*/
+        /* Skipping the first line containt CSV metadata */
         if (currLine > 0) {
+            /* Checking if line starts with semicolon, if so its an ingredient and the if statement does not run */
             if (line[0] != ';') {
                 currRecipeIndex++;
                 tempIngredientCount = 0;
-                
-                //int tempRecipeLength = strlen(line) - 4;
+
+                /* Copies the recipe name into the given structs recipeName variable */
                 snprintf(loadedRecipes[currRecipeIndex].recipeName, strlen(line) - 4, "%s", line);
-                //strncpy(loadedRecipes[currRecipeIndex].recipeName, line, tempRecipeLength);
                 strcat(loadedRecipes[currRecipeIndex].recipeName, "\0");
             }
             else {
+                /* Splits the line variable every time we encounter a semicolon */
 	            char *ingredientPtr = strtok(line, ingredientDelim);
                 while(ingredientPtr != NULL) {
                     ingredientPtr[strcspn(ingredientPtr, "\n")] = 0;
+                    /* Stores the ingredient name for the given ingredient */
                     if (tempIngredientInfoCount == 0) {
                         strcpy(loadedRecipes[currRecipeIndex].ingredients[tempIngredientCount].ingredientName, ingredientPtr);
                         tempIngredientInfoCount++;
                     }
+                    /* Stores the amount (Grams) for the given ingredient */
                     else if (tempIngredientInfoCount == 1) {
                         loadedRecipes[currRecipeIndex].ingredients[tempIngredientCount].amount = (unsigned short) atoi(ingredientPtr);
                         tempIngredientInfoCount++;
                     }
+                    /* Stores the weight for the given ingredient */
                     else if (tempIngredientInfoCount == 2) {
                         loadedRecipes[currRecipeIndex].ingredients[tempIngredientCount].weight = (unsigned short) atoi(ingredientPtr);
                         loadedRecipes[currRecipeIndex].ingredientCount++;
