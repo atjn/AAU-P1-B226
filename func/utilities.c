563d737932f535566fdf79ecb65fd935e6dc267d
Jesper276 <jespermadsenvejle@hotmail.dk>
2021-11-15 10:26:50 +0100

    made changes
    made do kapitaliser karaterene og tilføjede en comment
    
    Co-Authored-By: Arnar F. <Arnarfreyr29@gmail.com>

--- func/utilities.c
+++ func/utilities.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <ctype.h>
 
-/*
+/**
  * Flushes the input buffer.
  * This is useful when taking input from the user with `scanf`,
  * because unreadable input might be left in the buffer, causing an infinite loop.
@@ -14,10 +14,9 @@ void flushInput(){
     while((inputChar = getchar()) != '\n' && inputChar != EOF);
 }
 
-/*
-This function makes it uppercase and returns string
+/**
+This function capitalises the first character of a string.
 */
-
 const char* capitaliseFirst(char* string){
 
     string[0] = toupper(string[0]);
