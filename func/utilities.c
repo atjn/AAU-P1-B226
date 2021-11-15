18e0ed112b44786e955ece023f49dfd7fc989d11
Jesper276 <90216888+Jesper276@users.noreply.github.com>
2021-11-15 10:22:02 +0100

    Update func/utilities.c
    Co-authored-by: Anton <dev@atjn.dk>

--- func/utilities.c
+++ func/utilities.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <ctype.h>
 
-/*
+/**
  * Flushes the input buffer.
  * This is useful when taking input from the user with `scanf`,
  * because unreadable input might be left in the buffer, causing an infinite loop.
