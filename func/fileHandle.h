c505a6c28acf4d9b9b9ff6ed8218e1ff5feac20e
Anton <dev@atjn.dk>
2021-11-17 11:31:20 +0100

    Improve testing system (#27)

--- func/fileHandle.h
+++ func/fileHandle.h
@@ -1,4 +1,2 @@
-#include "../lib/CuTest-AAU/CuTest.h"
 
 Recipe * readFile(int *recipesNumber);
-CuSuite *readFileGetSuite();
--- func/ui.h
+++ func/ui.h
@@ -1,13 +1,6 @@
-#include "../lib/CuTest-AAU/CuTest.h"
 
 int requestRecipeName(Recipe* recipes, int recipeCount);
 int requestAmountOfPeople();
 void printListOfRecipes();
 int requestRecipeNumber();
 void printRecipe(Recipe, int);
-CuSuite *requestRecipeNameGetSuite();
-CuSuite *requestAmountOfPeopleGetSuite();
-CuSuite *printListOfRecipesGetSuite();
-CuSuite *requestRecipeNumberGetSuite();
-CuSuite *printRecipeGetSuite();
-
