689496742e0d8f472a505de7afd66439f1799241
Anton <dev@atjn.dk>
2021-11-18 09:57:38 +0100

    Improve test of capitaliseFirst (#34)

--- func/utilities.c
+++ func/utilities.c
@@ -24,6 +24,32 @@ const char* capitaliseFirst(char* string){
 }
 
 void testCapitaliseFirst(CuTest* testCase){
-    char string[] = "hello";
-    CuAssertStrEquals(testCase, "Hello", capitaliseFirst(string));
+
+    char string1[] = "hello";
+    CuAssertStrEquals(testCase, "Hello", capitaliseFirst(string1));
+
+    char string2[] = "hello _?";
+    CuAssertStrEquals(testCase, "Hello _?", capitaliseFirst(string2));
+
+    char string3[] = "Hello";
+    CuAssertStrEquals(testCase, "Hello", capitaliseFirst(string3));
+
+    char string4[] = "HELLO";
+    CuAssertStrEquals(testCase, "HELLO", capitaliseFirst(string4));
+
+    char string5[] = "&unicorns";
+    CuAssertStrEquals(testCase, "&unicorns", capitaliseFirst(string5));
+
+    char string6[] = "123";
+    CuAssertStrEquals(testCase, "123", capitaliseFirst(string6));
+
+    char string7[] = "";
+    CuAssertStrEquals(testCase, "", capitaliseFirst(string7));
+
+    char string8[] = " ";
+    CuAssertStrEquals(testCase, " ", capitaliseFirst(string8));
+
+    char string9[] = "   ";
+    CuAssertStrEquals(testCase, "   ", capitaliseFirst(string9));
+
 }
