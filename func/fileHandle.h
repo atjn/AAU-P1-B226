56ba33fb3cffca61b16651c0f0e967630c952368
atjn <dev@atjn.dk>
2021-11-23 08:40:45 +0100

    Remove unnecessary test code
    The test system works fine without these lines of code

--- func/fileHandle.h
+++ func/fileHandle.h
@@ -1,6 +1,4 @@
-#include "../lib/CuTest-AAU/CuTest.h"
 
 Recipe * readRecipe(int *recipesNumber);
 IngredientData * readIngredients(int *ingredientCount);
 Category * readCategories(int *categoryCount, IngredientData *ingredients);
-CuSuite *readFileGetSuite();
