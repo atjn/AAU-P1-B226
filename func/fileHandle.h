e64c8be306af038ff94c81c38e6e8630cb04dc32
Skrome <anders.nyby01@gmail.com>
2021-11-29 13:53:39 +0100

    Update fileHandle.h

--- func/fileHandle.h
+++ func/fileHandle.h
@@ -7,7 +7,7 @@
  * The function returns a pointer to the array of structs loaded
  * Using semicolons instead of commas in the .csv files (Thanks Microsoft)
  *
- * @param recipesNumber
+ * @param recipesNumber Number of recipes
  * @return Recipe*
  */
 Recipe * readRecipe(int *recipesNumber);
@@ -20,10 +20,11 @@ Recipe * readRecipe(int *recipesNumber);
  * The function also takes a pointer to an array of IngredientsData and an array of Category. These parameters are used to return the loaded ingredients and categories.
  * The function also takes a pointer to the integers ingredientsLength and categoriesLength. These parameters are used to return the length of the two arrays.
  *
- * @param dataPath
- * @param ingredients
- * @param categories
- * @param ingredientsLength
- * @param categoriesLength
+ * @param dataPath Path to data
+ * @param ingredients Ingredients of recipe
+ * @param categories Categories of recipe
+ * @param ingredientsLength Amount of ingredients of recipe
+ * @param categoriesLength Amount of categories of recipe
+ * @return void
  */
 void readIngredients(const char *, IngredientData **, Category **, int *, int *);
