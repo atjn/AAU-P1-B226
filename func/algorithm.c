137edbfab5fd934d2331a89e59c6296d9fe2fa02
atjn <dev@atjn.dk>
2021-12-03 14:28:56 +0100

    Last touches

--- func/algorithm.c
+++ func/algorithm.c
@@ -17,12 +17,12 @@ void scopeIngredientIndexes(int [], int [], int [], const Recipe *, const float,
 // The function takes the recipe, recipelist, and the parameter alternativeRecipes
 // in with the alternative recipes will be returned. The func also takes the ingredient list and its length.
 void makeListOfAlternativeRecipes(int recipeIndex, Recipe recipes[], Recipe alternativeRecipes[], IngredientData ingredients[], const int ingrNum, Category categories[], const int categoriesLength, const bool optimize){
-;
+
     const float accuracy = optimize ? 0.10 : 0.0;
     const float maxTime = optimize ? 2 : 5400;
-    bool targetCanBeSatisfied = true;
-    Recipe *originalRecipe = &recipes[recipeIndex];
 
+    bool canSatisfyTarget = true;
+    Recipe *originalRecipe = &recipes[recipeIndex];
     const float originalRecipeCoo = calculateRecipeCoo(originalRecipe, ingredients, ingrNum);
 
     // Generate list of targets
@@ -30,10 +30,9 @@ void makeListOfAlternativeRecipes(int recipeIndex, Recipe recipes[], Recipe alte
     float targets[RECIPES_IN_ALTERNATIVES_LIST];
     for(int t = 0; t < RECIPES_IN_ALTERNATIVES_LIST; t++){
         targets[t] = (float)(t+1) / (RECIPES_IN_ALTERNATIVES_LIST+1);
-        printf("targets %i: %lf", t, targets[t]);
     }
 
-    // Get this respective ingredientcategory from the original recipe
+    // Gets the respective ingredientcategories from the original recipe
     // also makes a list of the indexes of ingredients in each category list
     // the list of indexes is used to loop through every single combination of recipes
     int ingredientIndexes[originalRecipe->ingredientCount];
@@ -47,7 +46,8 @@ void makeListOfAlternativeRecipes(int recipeIndex, Recipe recipes[], Recipe alte
         ingredientIndexes[i] = 0;
     }
 
-    // This keeps track of the best combination of ingredients for each respective targets
+    // This keeps track of the best combination of ingredients for each respective target
+    // It is used if it is not possible to satisfy the target - then the closest match (stored here) is used instead
     IngredientData* bestIngredients[RECIPES_IN_ALTERNATIVES_LIST][originalRecipe->ingredientCount];
     float bestIngredientScores[RECIPES_IN_ALTERNATIVES_LIST];
     for (int i = 0; i < RECIPES_IN_ALTERNATIVES_LIST; i++) {
@@ -56,10 +56,10 @@ void makeListOfAlternativeRecipes(int recipeIndex, Recipe recipes[], Recipe alte
 
     for (int t = 0; t < RECIPES_IN_ALTERNATIVES_LIST; t++){
 
-        scopeIngredientIndexes(ingredientMaxIndexes, ingredientMinIndexes, ingredientStartIndexes, originalRecipe, originalRecipeCoo, ingredientCategories, targets[t], accuracy, &targetCanBeSatisfied, t, optimize);
+        scopeIngredientIndexes(ingredientMaxIndexes, ingredientMinIndexes, ingredientStartIndexes, originalRecipe, originalRecipeCoo, ingredientCategories, targets[t], accuracy, &canSatisfyTarget, t, optimize);
 
         if(DEBUG){
-            printf("\n\nNew target: %1.2lf\nCan satisfy target: %s", targets[t], targetCanBeSatisfied ? "yes" : "no");
+            printf("\n\nNew target: %1.2lf\nCan satisfy target: %s", targets[t], canSatisfyTarget ? "yes" : "no");
             printf("\n\nMin:   ");
             for(int i = 0; i < originalRecipe->ingredientCount; i++){
                 printf("%2i, ", ingredientMinIndexes[i]);
@@ -113,6 +113,7 @@ void makeListOfAlternativeRecipes(int recipeIndex, Recipe recipes[], Recipe alte
             // Calculate a score for the ingredients combination
             // if the score is better than any other score for a given target,
             // then save it as the best option for now
+            // if the ratio hits within the specified target, then save that recipe and quit immediately
             const float score = fabs(ratio-targets[t]);
 
             if(score < bestIngredientScores[t] && !duplicatedIngredients(ingredientCategories, ingredientIndexes, originalRecipe->ingredientCount)){
@@ -135,12 +136,15 @@ void makeListOfAlternativeRecipes(int recipeIndex, Recipe recipes[], Recipe alte
                 }
             }
 
-        }while(!triedAllCombinations && !foundMatchingIngredients && (!foundValidIngredients || (double)(clock()-start) < (maxTime/RECIPES_IN_ALTERNATIVES_LIST)*CLOCKS_PER_SEC));
+        }while(!triedAllCombinations && !foundMatchingIngredients && (canSatisfyTarget || !foundValidIngredients || (double)(clock()-start) < (maxTime/RECIPES_IN_ALTERNATIVES_LIST)*CLOCKS_PER_SEC));
 
+        //TODO: This output seems to be buffered and printed in larger batches, but that defeats the purpose of showing the user exactly how far the algorithm is
         if(!optimize) printf(" %.0lf%%", (double)((t+1)*100) / RECIPES_IN_ALTERNATIVES_LIST);
+
         if(DEBUG) printf("Finished in %3.2lfs, max allowed was %3.2lfs\n\n", (double)(clock()-start)/CLOCKS_PER_SEC, maxTime/RECIPES_IN_ALTERNATIVES_LIST);
 
         // This fails if the algorithm was not able to find even one possible recipe candidate
+        // This usually happens when there are too many ingredients with the same category and/or too few options available in the category
         assert(foundValidIngredients);
 
     }
@@ -158,6 +162,15 @@ void makeListOfAlternativeRecipes(int recipeIndex, Recipe recipes[], Recipe alte
     }
 }
 
+/**
+ * @brief checks if a list of ingredients contains duplicate ingredients
+ *
+ * @param ingredientCategories
+ * @param ingredientIndexes
+ * @param ingredientCount
+ * @return true
+ * @return false
+ */
 bool duplicatedIngredients(Category *ingredientCategories[], const int ingredientIndexes[], const int ingredientCount){
 
     bool duplicatedIngredients = false;
@@ -172,8 +185,24 @@ bool duplicatedIngredients(Category *ingredientCategories[], const int ingredien
     return duplicatedIngredients;
 }
 
+/**
+ * @brief uses a less intensive algorithm to remove ingredients that would never be able to satisfy the given target, before the main algorithm us run
+ *
+ * @param ingredientMaxIndexes - will return the max ingredient index that the main algorithm should check
+ * @param ingredientMinIndexes - will return the minimum ingredient index that the main algorithm should check
+ * @param ingredientStartIndexes - will return the recommended ingredient start index for the main algorithm (speculative)
+ * @param originalRecipe
+ * @param originalRecipeCoo
+ * @param ingredientCategories
+ * @param target
+ * @param accuracy
+ * @param targetCanBeSatisfied - will return whether or not it should be possible to satisfy the target
+ * @param ingredientCategoryIndex
+ * @param optimize
+ */
 void scopeIngredientIndexes(int ingredientMaxIndexes[], int ingredientMinIndexes[], int ingredientStartIndexes[], const Recipe *originalRecipe, const float originalRecipeCoo, Category *ingredientCategories[], const float target, const float accuracy, bool *targetCanBeSatisfied, const int ingredientCategoryIndex, const bool optimize){
 
+    // Reset all indexes
     for (int c = 0; c < originalRecipe->ingredientCount; c++){
         ingredientMinIndexes[c] = 0;
         ingredientMaxIndexes[c] = ingredientCategories[c]->ingredientCount-1;
@@ -181,9 +210,11 @@ void scopeIngredientIndexes(int ingredientMaxIndexes[], int ingredientMinIndexes
 
     if(!optimize) return;
 
+    // NOTE: These optimizations only work because the ingredients are sorted by Co2-emissions (least to most)
 
     for (int c = 0; c < originalRecipe->ingredientCount; c++){
 
+        // Remove all ingredients that emit so little CO2, they can't possibly hit the given target
         for(int i = ingredientMinIndexes[c]; i <= ingredientMaxIndexes[c]; i++){
 
             float coo = ingredientCategories[c]->ingredientData[i]->coo*originalRecipe->ingredients[c].amount;
@@ -202,6 +233,7 @@ void scopeIngredientIndexes(int ingredientMaxIndexes[], int ingredientMinIndexes
 
         }
 
+        // Remove all ingredients that emit so much CO2, they can't possibly hit the given target
         for(int i = ingredientMaxIndexes[c]; i >= ingredientMinIndexes[c]; i--){
 
             float coo = ingredientCategories[c]->ingredientData[i]->coo*originalRecipe->ingredients[c].amount;
@@ -219,13 +251,16 @@ void scopeIngredientIndexes(int ingredientMaxIndexes[], int ingredientMinIndexes
             }
         }
 
+        // If the min and max ingredient indexes have gone beyond each other, then it means it is impossible to hit the target
+        // In that case, set the indexes up for the algorithm to quickly find something as close as possible to the target
         if(ingredientMinIndexes[c] > ingredientMaxIndexes[c]){
             const int tempMin = ingredientMinIndexes[c];
             ingredientMinIndexes[c] = ingredientMaxIndexes[c];
             ingredientMaxIndexes[c] = tempMin;
             *targetCanBeSatisfied = false;
         }else if(ingredientMinIndexes[c] == ingredientMaxIndexes[c]){
-            //this is an import check, because without it, the algorithm could be forced to choose two duplicate ingredients
+            // This is an important check, because without it, the algorithm could be forced to choose two duplicate ingredients.
+            // TODO: The algorithm can still be forced to do this in extreme cases. This should be solved in a better way.
             if(ingredientMinIndexes[c] > 0){
                 ingredientMinIndexes[c] -= 1;
             }else if(ingredientMaxIndexes[c] < ingredientCategories[c]->ingredientCount-1){
@@ -233,6 +268,9 @@ void scopeIngredientIndexes(int ingredientMaxIndexes[], int ingredientMinIndexes
             }
         }
 
+        // The optimal ingredient combination is usually comprised of ingredients that are close to the middle of their respsective lists.
+        // Knowing this, ask the algorithm to start the search with the index of each ingredient 1/3 into the list.
+        // If no suitable target was found in the last 2/3 of the list, then the algorithm will also check the first 1/3 of the list.
         ingredientStartIndexes[c] = floor((ingredientMaxIndexes[c] - ingredientMinIndexes[c]) /RECIPES_IN_ALTERNATIVES_LIST *ingredientCategoryIndex)  +ingredientMinIndexes[c];
 
 
