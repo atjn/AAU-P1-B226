cbebec95743f4148d4d7ed043f0121fe8818220f
Skrome <anders.nyby01@gmail.com>
2021-12-03 10:15:08 +0100

    Fix Doxygen Logo

--- Doc/html/search/functions_5.js
+++ Doc/html/search/functions_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['printlistofalternativerecipes_0',['printListOfAlternativeRecipes',['../ui_8c.html#a0949269c60e56e3d9a6e797a2b69a67f',1,'printListOfAlternativeRecipes(Recipe alternativeRecipes[]):&#160;ui.c'],['../ui_8h.html#a3f31a25ed9b50fd73bbdbaca4fa7f891',1,'printListOfAlternativeRecipes(Recipe[]):&#160;ui.c']]],
+  ['printlistofalternativerecipes_0',['printListOfAlternativeRecipes',['../ui_8c.html#a8e8420da2665c90b30dd6a49b063fecd',1,'printListOfAlternativeRecipes(Recipe alternativeRecipes[], Recipe recipes[], const int recipeIndex, IngredientData ingredients[], const int ingredientsLength):&#160;ui.c'],['../ui_8h.html#ac5d641d4558f2cfc2b05807040cd8d3f',1,'printListOfAlternativeRecipes(Recipe[], Recipe[], const int, IngredientData[], const int):&#160;ui.c']]],
   ['printrecipe_1',['printRecipe',['../ui_8c.html#ab4fd0478609988d2e29237cd7bc0e766',1,'printRecipe(Recipe *recipe, const int people, IngredientData *ingredients, const int ingrNum):&#160;ui.c'],['../ui_8h.html#ab4fd0478609988d2e29237cd7bc0e766',1,'printRecipe(Recipe *recipe, const int people, IngredientData *ingredients, const int ingrNum):&#160;ui.c']]]
 ];
--- Doc/html/search/pages_0.js
+++ Doc/html/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['how_20to_20use_0',['HOW TO USE',['../md_lib__cu_test__a_a_u__r_e_a_d_m_e.html',1,'']]]
+  ['how_20to_20use_0',['HOW TO USE',['../md__a_a_u__p1__b226_lib__cu_test__a_a_u__r_e_a_d_m_e.html',1,'']]]
 ];
